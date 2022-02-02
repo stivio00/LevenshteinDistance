31a836a0e5c22e65a01c2a8582bf3542a5d9eecd
Stephen Krol <krol.stephen@gmail.com>
2022-02-02 18:15:07 +0100

    added factories

--- src/main/java/me/stephenk/EditOperations/AtomicOperation.java
+++ src/main/java/me/stephenk/EditOperations/AtomicOperation.java
@@ -1,4 +1,16 @@
 package me.stephenk.EditOperations;
 
 public record AtomicOperation(OperationType type, int index, String value) {
+
+    public static AtomicOperation newInsert(int index, String value){
+        return new AtomicOperation(OperationType.insert, index, value);
+    }
+
+    public static AtomicOperation newDelete(int index, String value){
+        return new AtomicOperation(OperationType.delete, index, value);
+    }
+
+    public static AtomicOperation newSubstitution(int index, String value){
+        return new AtomicOperation(OperationType.substitution, index, value);
+    }
 }
